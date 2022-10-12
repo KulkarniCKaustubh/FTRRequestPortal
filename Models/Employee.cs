f4f5b24469896559a86b19996ea5e940093ba5f9
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-10-12 13:04:18 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FTRRequestPortal.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public string Emp_Id { get; set; }
+
+        [Required(ErrorMessage = "Employee Name is required")]
+        public string Emp_Name { get; set; }
+
+        [Required(ErrorMessage = "Manager is required")]
+        public string MGR { get; set; }
+
+        [Required(ErrorMessage = " Home City is required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Home Country is required")]
+        public string Country { get; set; }
+
+        [ForeignKey("GCM")]
+        [Required (ErrorMessage = " GCM LEVEL is required")]
+        public int GCM_Id { get; set; }
+
+        [Required(ErrorMessage = "DasId is required")]
+        public string DAS_Id { get; set; }  
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/Travel_Details.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FTRRequestPortal.Models
+{
+    public class Travel_Details
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public  int Request_No { get; set; }
+
+        [ForeignKey("Employee")]
+        public string Emp_Id { get; set; }
+
+        [ForeignKey("GCM")]
+        [Required(ErrorMessage = "GCM Level is required")]
+        public int GCM_Id { get; set; }
+
+        [ForeignKey("TRAVEL")]
+        [Required(ErrorMessage = "Travel Type  is required")]
+        public int Travel_Id { get; set; }
+
+        [ForeignKey("Country")]
+        [Required(ErrorMessage = "Host Country is required")]
+        public string country_Id { get; set; }
+
+        [ForeignKey("City")]
+        [Required(ErrorMessage = "Host City required")]
+        public  int City_Id { get; set; }
+
+        [Required(ErrorMessage = "Satart Date is required")]
+        public DateTime Start_Date { get; set; }
+
+        [Required(ErrorMessage = "Expected Date is required")]
+        public DateTime Expected_EndDate { get; set; }
+
+
+        [ForeignKey("Employee")]
+         [Required(ErrorMessage = "Manager is required")]
+        public string MGR { get; set; }
+
+        public string status  { get; set; }
+
+
+
+
+    }
+}
